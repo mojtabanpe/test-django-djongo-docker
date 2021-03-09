6d8799c3edbc6a5ef31715295842730b1da4662f
root <root@vm3143583848.bitcommand.com>
2021-03-09 11:56:00 +0330

    server-test-djongo

--- django_mongodb_docker/settings.py
+++ django_mongodb_docker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hgvr2wxx74)l=!mtx((y3z*_w1kj&x%ken-e5gyd@u0)yd@g9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
